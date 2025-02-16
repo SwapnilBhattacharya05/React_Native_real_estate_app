320f87d42db474995fe930f9d1c2638e200a467a
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-02-16 16:37:00 +0530

    Design Sign In Screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
@@ -19,15 +18,17 @@ module.exports = {
           200: "#0061FF1A",
           300: "#0061FF",
         },
-        accent: "#FBFBFD",
+        accent: {
+          100: "#FBFBFD",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#666876",
+          300: "#191D31",
+        },
+        danger: "#F75555",
       },
-      black: {
-        DEFAULT: "#000000",
-        100: "#8C8E98",
-        200: "#666876",
-        300: "#191d31",
-      },
-      danger: "#F75555"
     },
   },
   plugins: [],
